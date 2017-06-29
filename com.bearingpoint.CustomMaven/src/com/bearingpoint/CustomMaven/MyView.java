cd1ce35e94a5880192dfde8d27933fb514d39f60
borshnicolae <borsh.nicolae@yahoo.com>
2017-06-29 12:32:51 +0300

    Maven Plugin

---
+++ com.bearingpoint.CustomMaven/src/com/bearingpoint/CustomMaven/MyView.java
@@ -0,0 +1,19 @@
+package com.bearingpoint.CustomMaven;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MyView extends ViewPart {
+
+
+    @Override
+    public void createPartControl(Composite parent) {
+    	new Comp(parent, SWT.NONE);
+    }
+
+    @Override
+    public void setFocus() {
+    }
+    
+}
---
+++ com.bearingpoint.CustomMaven/src/com/bearingpoint/CustomMaven/PerspectiveFactory1.java
@@ -0,0 +1,12 @@
+package com.bearingpoint.CustomMaven;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveFactory1 implements IPerspectiveFactory {
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+ 
+	}
+}
